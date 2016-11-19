57162d9ebd94919d79e26b4fc45039fd9862a5d1
sdowell <sdowell@umail.ucsb.edu>
2016-11-19 15:25:00 -0800

    Created main with training function

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ import sys
 
 def parseTrainFile(trainfile):
 	with open(trainfile) as f:
-		content = [x.split("\t") for line in f]
+		content = [line.split("\t") for line in f]
 	return content
 	
 
 def train(weights, trainfile):
-	return
+	results = {}
+	for w in weights:
+		d = disambiguator(w)
+		pos = 0
+		tot = 0
+		for t in trainfile:
+			e = d.disambiguate(t)
+			tot++
+			#if its correct, add 1 to pos
+	return results
 
 def main(argv):
 	if len(argv) == 0:
@@ -15,7 +24,7 @@ def main(argv):
 		return
 	trainfile = argv[0]
 	traindata = parseTrainFile(trainfile)
-	print(traindata)
+	print(traindata[0:10])
 	weights = [0.0,.1,.2,.3,.4,.5,.6,.7,.8,.9,1.0]
 	train(weights, trainfile)
 
